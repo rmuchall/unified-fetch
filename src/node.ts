b07fc716bb0c3a47a8f615ac6d6bc661e0b2eb6d
Rob <rmuchall@protonmail.com>
2021-07-29 04:51:44 +0100

    Updated dependencies

--- src/node.ts
+++ src/node.ts
@@ -1,10 +1,10 @@
 import nodeFetch from "node-fetch";
 
 // https://github.com/node-fetch/node-fetch/tree/3.x#custom-highwatermark
-global.fetch = (input: RequestInfo, init?: RequestInit) => nodeFetch(input as any, {...init as any, ...{ highWaterMark: 10 }});
-global.Headers = nodeFetch.Headers;
-global.Request = nodeFetch.Request;
-global.Response = nodeFetch.Response;
+global.fetch = (input: RequestInfo, init?: RequestInit) => nodeFetch(input as any, {...init as any, ...{ highWaterMark: 10 }}) as unknown as Promise<Response>;
+global.Headers = nodeFetch.Headers as any;
+global.Request = nodeFetch.Request as any;
+global.Response = nodeFetch.Response as any;
 
 /*
 global.AbortController = ? // Requires another shim - not included (yet)
